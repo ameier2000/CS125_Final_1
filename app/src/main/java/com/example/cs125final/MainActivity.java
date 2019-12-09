6559975166f026ebe6793be27591ec180c6cf658
Aidan <ameier3@illinois.edu>
2019-12-09 16:48:01 -0600

    close

--- app/src/main/java/com/example/cs125final/MainActivity.java
+++ app/src/main/java/com/example/cs125final/MainActivity.java
@@ -1,18 +1,53 @@
 package com.example.cs125final;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static java.net.Proxy.Type.HTTP;
+
+
 public class MainActivity extends AppCompatActivity {
-    private String playerOneName;
-    private String playerTwoName;
     private int playerdealer = 1;
     private int ante = 250;
     private Integer playerOneSum = 5000;
@@ -51,6 +86,8 @@ public class MainActivity extends AppCompatActivity {
         TextView questions = findViewById(R.id.questions);
         questions.setText("Bet (Min: 250)   Raise (Min: " + prevBet.toString() + ")");
         EditText input = findViewById(R.id.userInput);
+
+
         pot = 500;
         playerOneSum -= 250;
         playerTwoSum -= 250;
@@ -343,24 +380,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateValues(int turn) {
+
          if (turn == 1) {
              TextView playerinfo = findViewById(R.id.playerInfo);
-             playerinfo.setText("                Player 1's Turn");
+             playerinfo.setText("                  Player 1's Turn");
          }
          else {
              TextView playerinfo = findViewById(R.id.playerInfo);
-             playerinfo.setText("                Player 2's Turn");
+             playerinfo.setText("                  Player 2's Turn");
          }
+
+         doInBackground();
          TextView playerval = findViewById(R.id.currentPlayerVal);
          playerval.setText("     P1 $" + playerOneSum.toString() + "         P2 $" + playerTwoSum.toString());
          TextView potval = findViewById(R.id.potVal);
          potval.setText("       Pot: " + pot.toString());
-        TextView questions = findViewById(R.id.questions);
-        questions.setText("Bet (Min: 250)   Raise (Min: " + prevBet.toString() + ")");
+         TextView questions = findViewById(R.id.questions);
+         questions.setText("Bet (Min: 250)   Raise (Min: " + prevBet.toString() + ")");
 
     }
+    private String facttime() throws Exception {
+        URL url = new URL("https://api.chucknorris.io/jokes/random");
+        URLConnection con = url.openConnection();
+        con.connect();
 
-    private void getName() {
+        JsonParser jp = new JsonParser(); //from gson
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) con.getContent())); //Convert the input stream to a json element
+        JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+        String joke = rootobj.get("value").getAsString();
+        return joke;
+    }
+    public void doInBackground() {
+        TextView c = findViewById(R.id.chuckNorris);
+        c.setText("Getting new fact...");
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.chucknorris.io/jokes/random";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String joke = response.getString("value");
+                            c.setText(joke);
+                        } catch (Exception e) {
+                            c.setText(e.toString());
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        c.setText("error");
+
+                    }
+                });
+        queue.add(jsonObjectRequest);
+        }
+
 
     }
-}
